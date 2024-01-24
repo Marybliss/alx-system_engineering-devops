ce35f77fb397a5ee07b21ed1a498386a78b93c4e
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2024-01-24 22:12:48 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create file in /tmp
-
-file { '/tmp/school' :
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-    }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# create a process named killmenow 
-
-exec { 'pkill killmenow' :
-    path    => '/bin/',
-    command => 'pkill killmenow',
-    }
